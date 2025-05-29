a24775cef7b89dd49a4972af052eb60efb8fd08c
Kanishk Gandhi <kanishk050296@gmail.com>
2025-05-29 01:20:38 -0700

    debug

--- verl/workers/fsdp_workers.py
+++ verl/workers/fsdp_workers.py
@@ -388,6 +388,8 @@ class ActorRolloutRefWorker(Worker):
 
         # TODO(sgm): support FSDP hybrid shard for larger model
         infer_tp = self.config.rollout.tensor_model_parallel_size
+        self.world_size = 8
+        infer_tp = 2
         dp = self.world_size // infer_tp
         assert self.world_size % infer_tp == 0, f"rollout world_size: {self.world_size} is not divisible by infer_tp: {infer_tp}"
         rollout_device_mesh = init_device_mesh(device_name, mesh_shape=(dp, infer_tp), mesh_dim_names=["dp", "infer_tp"])
